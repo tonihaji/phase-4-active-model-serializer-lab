53bae20ed6310519fb155f5ca6800f2725ee3546
ehachi <ehajianto@gmail.com>
2023-03-22 20:53:20 +0300

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,12 +3,16 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all 
-    render json: authors
+    render json: authors, include: ['profile', 'posts', 'posts.tags']
   end
 
   def show
-    author = Author.find(params[:id])
-    render json: author
+    author = Author.find_by(id: params[:id])
+    if author
+      render json: author, include: ['profile', 'posts', 'posts.tags']
+    else 
+      render json: { errors: "Author not found" }
+    end
   end
 
   private
